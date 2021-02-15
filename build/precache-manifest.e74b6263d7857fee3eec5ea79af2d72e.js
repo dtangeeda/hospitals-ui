19cd90f1bd6e587ac91bea503909511715e910c7
masroorali <masroor748@gmail.com>
2021-02-15 19:54:36 +0500

    Update Build files

---
+++ build/precache-manifest.e74b6263d7857fee3eec5ea79af2d72e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d40f0daeead35193c71111b3deeaf2",
+    "url": "/dtangeeda/hospitals-ui/index.html"
+  },
+  {
+    "revision": "9b66a338e13bf5d4f15d",
+    "url": "/dtangeeda/hospitals-ui/static/css/2.09cfaa7c.chunk.css"
+  },
+  {
+    "revision": "84c425cf1441eb62d7f0",
+    "url": "/dtangeeda/hospitals-ui/static/css/main.e3de534e.chunk.css"
+  },
+  {
+    "revision": "9b66a338e13bf5d4f15d",
+    "url": "/dtangeeda/hospitals-ui/static/js/2.48ee2f7c.chunk.js"
+  },
+  {
+    "revision": "466797eeb1ac31dc75ba0fc29e9991eb",
+    "url": "/dtangeeda/hospitals-ui/static/js/2.48ee2f7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84c425cf1441eb62d7f0",
+    "url": "/dtangeeda/hospitals-ui/static/js/main.0e97075b.chunk.js"
+  },
+  {
+    "revision": "15fe9b15e62bccbfb9dd",
+    "url": "/dtangeeda/hospitals-ui/static/js/runtime-main.5de7f55f.js"
+  },
+  {
+    "revision": "293ee857ae236bbb94d621fcd4c7b10d",
+    "url": "/dtangeeda/hospitals-ui/static/media/Blog.293ee857.png"
+  },
+  {
+    "revision": "e0ecf43b9ef0c8d33bc1c32cb4760439",
+    "url": "/dtangeeda/hospitals-ui/static/media/Logo1.e0ecf43b.png"
+  },
+  {
+    "revision": "50c50fb41fe810f30edf5366e053db58",
+    "url": "/dtangeeda/hospitals-ui/static/media/card1.50c50fb4.png"
+  },
+  {
+    "revision": "df9993291d93b6f9ec99a30071f92d08",
+    "url": "/dtangeeda/hospitals-ui/static/media/card2.df999329.png"
+  },
+  {
+    "revision": "b9d9ef7d8846b90bd4d3dde1d1542468",
+    "url": "/dtangeeda/hospitals-ui/static/media/card3.b9d9ef7d.png"
+  },
+  {
+    "revision": "a9f1b1f688d1e749fa8c246112304738",
+    "url": "/dtangeeda/hospitals-ui/static/media/contactbg.a9f1b1f6.png"
+  },
+  {
+    "revision": "a11dc554eae3d9352f18012930e3bf64",
+    "url": "/dtangeeda/hospitals-ui/static/media/footerlogo.a11dc554.svg"
+  },
+  {
+    "revision": "66da5e74f161ea0308997a09ddf636d1",
+    "url": "/dtangeeda/hospitals-ui/static/media/home.66da5e74.png"
+  }
+]);
